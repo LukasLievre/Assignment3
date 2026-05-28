21b395cf5f84efcd4ee8bb5002e24085024de3c0
braluba <lukasbannies@gmail.com>
2026-05-28 16:53:56 +0200

    Aufgabe 3a) Funktionen hoffentlich erfolgreich geschrieben.

--- src/de/uni_potsdam/mon/ds/clocks/Clock.java
+++ src/de/uni_potsdam/mon/ds/clocks/Clock.java
@@ -12,9 +12,9 @@ public class Clock extends Thread {
 	private final String name;
 
 	public Clock(String name, long incrementTime, double drift) {
-		super("Clock " + name);
+		super("Clock " + name); //allocates a new thread object
 		counter = 0L;
-		this.incrementTime = incrementTime;
+		this.incrementTime = incrementTime; //time between two clock ticks (in main(), it is 50 ms)
 		this.drift = drift;
 		this.name = name;
 		this.start();
@@ -22,14 +22,23 @@ public class Clock extends Thread {
 
 	public synchronized long getTime() {
 		// TODO: Implement/modify me!
-		return 0;
+		return (counter*incrementTime) + (long) (counter*(drift+1));
 	}
 
 	public synchronized void setTime(long time) {
 		// TODO: Implement me!
-	}
+		this.counter = time;
+		}
 
 	public void run() {
 		// TODO: Implement me!
+		//this method imitates the time
+		while (true) {
+			counter +=1;
+			try {
+				Clock.sleep(incrementTime);
+			} catch (InterruptedException ignored) {
+			}
+		}
 	}
 }
