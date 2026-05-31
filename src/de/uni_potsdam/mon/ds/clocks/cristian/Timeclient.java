6585e36db8667bb527b6931588b165354f68b78e
LukasLievre <lukas.bannies@gmail.com>
2026-05-31 17:28:34 +0200

    Final commit

--- src/de/uni_potsdam/mon/ds/clocks/cristian/Timeclient.java
+++ src/de/uni_potsdam/mon/ds/clocks/cristian/Timeclient.java
@@ -12,7 +12,7 @@ public class Timeclient implements Runnable {
 	private VSDatagramSocket clientSocket;
 	private String name;
 	private final static long delta = 1000; //maximum delta between two clocks
-	private final static double maximal_drift_rate = 0.0d; // TODO: Correct me
+	private final static double maximal_drift_rate = 0.16d; // TODO: Correct me
 
 	public Timeclient(Clock clock, InetSocketAddress address, String name) {
 		this.clock = clock;
@@ -21,14 +21,35 @@ public class Timeclient implements Runnable {
 	}
 
 	public void run() {
-		// TODO: Implement me!
+
+		InetSocketAddress serverAddr = new InetSocketAddress("localhost", 4000);
+
 		while (true) {
-
 			try {
-				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-			} catch (Exception e) {
-                throw new RuntimeException(e);
+				// starting time
+				long t0 = clock.getTime();
+				DatagramPacket request = new DatagramPacket(new byte[1], 1, serverAddr);
+				clientSocket.send(request);
 
-            }
-	}}
+				DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+				clientSocket.receive(response);
+
+				// end point of measurement
+				long t1 = clock.getTime();
+
+				long serverTime = java.nio.ByteBuffer.wrap(response.getData(), 0, response.getLength()).getLong();
+
+				// actual algorithm
+				long rtt = t1 - t0;
+				long newTime = serverTime + (rtt / 2);
+				clock.setTime(newTime);
+
+				// wait for the intervall
+				Thread.sleep(3125); //answer for c but already computed (delta/2c)
+
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 }
--- src/de/uni_potsdam/mon/ds/clocks/cristian/Timeserver.java
+++ src/de/uni_potsdam/mon/ds/clocks/cristian/Timeserver.java
@@ -17,15 +17,24 @@ public class Timeserver implements Runnable {
 	}
 
 	public void run() {
-		// TODO: Implement me
 		while (true) {
+			try {
+				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+				serverSocket.receive(packet); // Hier füllt Java die Absender-Adresse in 'packet'
 
-				try {
-					DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-				} catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+				// current correct time
+				long currentTime = clock.getTime();
 
+
+				byte[] timeBytes = java.nio.ByteBuffer.allocate(8).putLong(currentTime).array();
+
+
+				DatagramPacket response = new DatagramPacket(timeBytes, timeBytes.length, packet.getSocketAddress());
+				serverSocket.send(response);
+
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
